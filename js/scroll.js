2b855f187b2b5690814fa970d4e861658dbc0027
Jasmyn <jasmyn.marasigan@gmail.com>
2017-03-10 00:42:13 -0800

    added sticky navbar for bubbletea categories when scrolling

--- js/scroll.js
+++ js/scroll.js
@@ -1,22 +1,44 @@
 //Scroll code taken from http://stackoverflow.com/questions/19012495/smooth-scroll-to-div-id-jquery
+//Using media queries for jQuery taken from https://www.sitepoint.com/javascript-media-queries/
 
 // handle links with @href started with '#' only
 $(document).on('click', 'a[href^="#"]', function(e) {
-    // target element id
-    var id = $(this).attr('href');
+	var mq = window.matchMedia( "(min-width: 50em)" );
+		if (mq.matches) {
+		    // target element id
+		    var id = $(this).attr('href');
 
-    // target element
-    var $id = $(id);
-    if ($id.length === 0) {
-        return;
-    }
+		    // target element
+		    var $id = $(id);
+		    if ($id.length === 0) {
+		        return;
+		    }
 
-    // prevent standard hash navigation (avoid blinking in IE)
-    e.preventDefault();
+		    // prevent standard hash navigation (avoid blinking in IE)
+		    e.preventDefault();
 
-    // top position relative to the document
-    var pos = $id.offset().top;
+		    // top position relative to the document
+		    var pos = $id.offset().top-80;
 
-    // animated top scrolling
-    $('body, html').animate({scrollTop: pos});
+		    // animated top scrolling
+		    $('body, html').animate({scrollTop: pos});
+		} else {
+		    // target element id
+		    var id = $(this).attr('href');
+
+		    // target element
+		    var $id = $(id);
+		    if ($id.length === 0) {
+		        return;
+		    }
+
+		    // prevent standard hash navigation (avoid blinking in IE)
+		    e.preventDefault();
+
+		    // top position relative to the document
+		    var pos = $id.offset().top-200;
+
+		    // animated top scrolling
+		    $('body, html').animate({scrollTop: pos});
+		}
 });
